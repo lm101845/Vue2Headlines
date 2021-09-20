9129777b0a0a1103afcda776ed1ddad9be6331ea
lm101845 <1018457683@qq.com>
2021-09-20 12:18:51 +0800

    Auto commit.

--- 02-代码手敲/headlines-m/src/App.vue
+++ 02-代码手敲/headlines-m/src/App.vue
@@ -1,22 +1,27 @@
 <!--
  * @Author: liming
  * @Date: 2021-09-19 13:54:11
- * @LastEditTime: 2021-09-19 21:06:06
- * @FilePath: \02-代码手敲\headlines-m\src\App.vue
+ * @LastEditTime: 2021-09-20 12:02:42
+ * @FilePath: \headlines-m\src\App.vue
 -->
 <template lang="pug">
-    div#app
-       h1 黑马头条
-        //- 路由的出口，路由出口不要删
-        <router-view/>
-
+#app
+    h1 黑马头条
+    //- 路由的出口，路由出口不要删
+    <router-view/>
+    van-button(type="primary") 主要按钮
+    van-button(type="info") 信息按钮
+    van-button(type="default") 默认按钮
+    van-button(type="warning") 警告按钮
+    van-button(type="danger") 危险按钮
+    <van-cell-group inset/>
+        <van-cell title="单元格" value="内容" />
+        <van-cell title="单元格" value="内容" label="描述信息" />
 </template>
 
 <script>
-export default {
-    name:'App'
-}
+  export default {
+    name: "App",
+  };
 </script>
-<style lang="stylus">
-
-</style>
+<style lang="stylus"></style>
--- 02-代码手敲/headlines-m/src/main.js
+++ 02-代码手敲/headlines-m/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: liming
  * @Date: 2021-09-19 13:54:11
- * @LastEditTime: 2021-09-20 11:02:26
+ * @LastEditTime: 2021-09-20 11:49:16
  * @FilePath: \headlines-m\src\main.js
  */
 import Vue from 'vue'
@@ -10,6 +10,16 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.stylus'
+
+// 建议为了前期开发的便利性我们选择方式三：导入所有组件，在最后做打包优化的时候根据需求配置按需加载以降低打包体积大小。
+// 加载Vant组件库
+import Vant from 'vant';
+// 加载Vant组件库样式
+import 'vant/lib/index.css';
+// 全局注册Vant中的组件
+Vue.use(Vant);
+
+
 // 关闭生产环境提示
 Vue.config.productionTip = false
 // 创建Vue根实例，将router,store配置到根实例中
